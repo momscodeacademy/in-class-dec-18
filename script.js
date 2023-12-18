98f8a96147b9db160439b2f41a4ef679f5d0be9b
Alicia Wood <alicia.crowder@gmail.com>
2023-12-18 12:25:32 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function findMax(myList) {
+    
+}
+  
+  const listOfNumbers = [12, 4, 36, 81, 2, 7];
+  const max = findMax(numbersToMultiply);
+  console.log(`The highest number in this list of numbers is: ${max}`);
